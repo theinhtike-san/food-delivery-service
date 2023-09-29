1803a6ad71ce711bbf01c0a6dcfb808ff1209456
theinhtike-san <theinhtikesan.personal@gmail.com>
2023-09-29 16:21:01 +0630

    static

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.{html,css,js}",
+    "index.html"],
+  theme: {
+    fontFamily: {
+      'agdasima': ['Agdasima', 'sans- serif'],
+      'freehand': ['Freehand', 'cursive'],
+      'roboto': ['Roboto', 'sans - serif'],
+    },
+
+    extend: {
+      colors: {
+        primary: {
+          100: "#f7d3d3",
+          200: "#efa7a7",
+          300: "#e77b7b",
+          400: "#df4f4f",
+          500: "#d72323",
+          600: "#ac1c1c",
+          700: "#811515",
+          800: "#560e0e",
+          900: "#2b0707"
+        }
+      },
+    },
+  },
+  plugins: [],
+}
+
